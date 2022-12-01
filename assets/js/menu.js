f5307d1379e805e0272e8700efdf3a61152ab7dd
Alessandro Muratore <muratore@gmail.com>
2022-12-01 11:15:31 -0300

    Adicionado responsividade

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,17 +1,31 @@
 const btnHamburguer = document.querySelector('.hamburguer');
 const menuDesk = document.querySelector('nav ul')
 const menuLi = document.querySelectorAll('nav ul li')
+const arrowTop = document.querySelectorAll('h2>span')
+console.log(arrowTop);
 
 
 btnHamburguer.addEventListener('click', ()=>{
-    console.log('clicou');
-    menuDesk.classList.toggle('menu-mobile')
-    
+    menuDesk.classList.toggle('mobile')
 })
 
 
 for (const li of menuLi) {
-    li.addEventListener('click', ()=>{
-        menuDesk.classList.remove('menu-mobile')
+    li.addEventListener('click', (evt)=>{
+        menuDesk.classList.remove('mobile')
+        const targetSection = evt.target.innerText.toLowerCase();
+        console.log(targetSection);
+        goToView(`${targetSection}`)
     })
+}
+
+for (const arrow of arrowTop) {
+    arrow.addEventListener('click', (evt)=>{
+        goToView("topo")
+    })
+}
+
+const goToView = (target)=>{
+    console.log(target);
+    document.getElementById(target).scrollIntoView({behavior: 'smooth'});
 }
